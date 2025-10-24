f3c284b1b6b65cfef93283a7aa736f7b72473bcd
Zara <zlatozara@gmail.com>
2025-10-24 13:44:32 +0200

    Add clear completed button functionality: - Add clear completed button UI - Implement clear completed button logic - Style the clar completed button

--- script.js
+++ script.js
@@ -25,3 +25,10 @@ list.addEventListener('dblclick', e => {
     e.target.remove();
   }
 });
+
+// Clear all completed tasks
+const clearBtn = document.getElementById('clear-completed-btn');
+clearBtn.addEventListener('click', () => {
+  document.querySelectorAll('#task-list li.completed')
+    .forEach(li => li.remove());
+});
