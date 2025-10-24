e5a89d203026a5ea4f64c66ba417eb105590bc33
Zara <zlatozara@gmail.com>
2025-10-24 14:07:47 +0200

    Refactor event handlers

--- script.js
+++ script.js
@@ -25,3 +25,14 @@ list.addEventListener('dblclick', e => {
     e.target.remove();
   }
 });
+
+// Clear all completed tasks
+const clearBtn = document.getElementById('clear-completed-btn');
+clearBtn.addEventListener('click', () => {
+  document.querySelectorAll('#task-list li.completed')
+    .forEach(li => li.remove());
+});
+
+// Branch 2
+
+// Branch 3
